698c83a616733b83b416f97fcebcad9f24157e2d
Matthew Castillo <blacksheep3y@gmail.com>
2020-01-31 15:00:41 -0500

    Added the changes to the files but did not finish

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -102,7 +102,10 @@ const contactInfo = graduates.map(graduates => ({
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.map(graduates => ({
+  university: graduates.university.includes('Uni')
+}));
+
 console.log(unisWithUni);
 
 
@@ -128,7 +131,10 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+// .forEach() I DID NOT KNOW HOW TO GET THE FOR EACH TO RETURN....
+const displayNames = zooAnimals.map(zooAnimals => ({
+  displayNames: `Name: ${zooAnimals.animal_name} Scientific: ${zooAnimals.scientific_name}`
+}));
 console.log(displayNames);
 
 /* Request 2: .map()
